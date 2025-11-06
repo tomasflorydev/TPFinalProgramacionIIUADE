c49d02f673567c7cc1e3ba8993499eca3c291a8e
tomasflorydev <florytomas@protonmail.com>
2025-11-06 12:09:10 -0300

    feat: clase Persona

---
+++ src/Modelado/Persona.java
@@ -0,0 +1,32 @@
+package Modelado;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getEdad() { return edad; }
+
+    @Override
+    public String toString() {
+        return nombre + "(" + edad + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Persona)) return false;
+        Persona p = (Persona) o;
+        return edad == p.edad && nombre.equals(p.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return nombre.hashCode() * 31 + edad;
+    }
+}
